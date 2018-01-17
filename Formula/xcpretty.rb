c69cb694cb5d7b423d7ee429c76c2a634638f97f
Gregor Jasny <gregor.jasny@logmein.com>
2018-01-17 17:48:32 +0100

    WIP

--- Formula/xcpretty.rb
+++ Formula/xcpretty.rb
@@ -1,43 +1,22 @@
 class Xcpretty < Formula
+  desc "Flexible and fast xcodebuild formatter"
   homepage "https://github.com/supermarin/xcpretty"
   url "https://github.com/supermarin/xcpretty/archive/v0.2.8.tar.gz"
   sha256 "9f99f74d0e0540ea8573fdd7f1da1ab74e6b0f2d5860713442ac456a000362d8"
 
-  patch :DATA
+  bottle do
+    cellar :any
+  end
 
   def install
-    prefix.install "vendor"
-    prefix.install "lib" => "rubylib"
-
-    bin.install "bin/xcpretty"
+    ENV["GEM_HOME"] = libexec
+    system "gem", "build", "xcpretty.gemspec"
+    system "gem", "install", "xcpretty-#{version}.gem"
+    bin.install libexec/"bin/xcpretty"
+    bin.env_script_all_files(libexec/"bin", :GEM_HOME => ENV["GEM_HOME"])
   end
 
   test do
     system "#{bin}/xcpretty", "--version"
   end
 end
-
-__END__
-diff --git a/bin/xcpretty b/bin/xcpretty
-index c42f1b0..11bde17 100755
---- a/bin/xcpretty
-+++ b/bin/xcpretty
-@@ -1,12 +1,14 @@
--#!/usr/bin/env ruby
-+#!/usr/bin/env ruby --disable-gems
- 
- if RUBY_VERSION < '2.0.0'
-   abort "error: XCPretty requires Ruby 2.0.0 or higher."
- end
- 
--if $PROGRAM_NAME == __FILE__
--  $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
--end
-+require 'pathname'
-+
-+file_path = Pathname.new(__FILE__).realpath
-+$LOAD_PATH.unshift(File.expand_path("../../rubylib", file_path))
-+
- require 'xcpretty'
- require 'optparse'
- 
