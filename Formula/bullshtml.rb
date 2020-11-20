4b7270bc22952c702a0f89657d7f4526dcf04939
Gregor Jasny <gjasny@googlemail.com>
2020-11-20 17:47:10 +0100

    bullsview: use 1.2.1

--- Formula/bullshtml.rb
+++ Formula/bullshtml.rb
@@ -2,18 +2,11 @@ class Bullshtml < Formula
   desc "Html Reporter for BullseyeCoverage tool."
   homepage "https://github.com/RafalSkorka/bullshtml"
   url "https://github.com/RafalSkorka/bullshtml.git",
-      :tag => "1.2.0",
-      :revision => "43efec2ff5ea7db9622c679616908aeacf46e7b8"
-  revision 1
+      :tag => "1.2.1",
+      :revision => "6c2d0d7182b91a32b3c6d188ea4113f2047b7377"
 
   depends_on "ant" => :build
   depends_on "openjdk"
-
-  # Apply fix for https://github.com/RafalSkorka/bullshtml/pull/27
-  patch do
-    url "https://github.com/gjasny/bullshtml/commit/15eac03fba05676a562f50542c189b5699429626.diff?full_index=1"
-    sha256 "a3e470408919e90c56e40ab75b8da1009afb35353ccecb7379fa79fd06a69bf2"
-  end
 
   def install
     system "ant", "make_onejar", "-Ddebuglevel=none"
