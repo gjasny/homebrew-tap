e44bf49c96f21fdd6021342e1ce1c5f48e199f69
Gregor Jasny <gregor.jasny@logmein.com>
2021-11-20 13:06:10 +0100

    feat(git-lfs): Disable documentation generation

--- Formula/git-lfs.rb
+++ Formula/git-lfs.rb
@@ -4,10 +4,9 @@ class GitLfs < Formula
   url "https://github.com/git-lfs/git-lfs/releases/download/v2.13.3/git-lfs-v2.13.3.tar.gz"
   sha256 "f8bd7a06e61e47417eb54c3a0db809ea864a9322629b5544b78661edab17b950"
   license "MIT"
+  revision 1
 
   depends_on "go" => :build
-  depends_on "ronn" => :build
-  depends_on "ruby" => :build
 
   def install
     ENV["GIT_LFS_SHA"] = ""
@@ -17,12 +16,8 @@ class GitLfs < Formula
     cd "src/github.com/git-lfs/git-lfs" do
       system "make", "vendor"
       system "make"
-      system "make", "man", "RONN=#{Formula["ronn"].bin}/ronn"
 
       bin.install "bin/git-lfs"
-      man1.install Dir["man/*.1"]
-      man5.install Dir["man/*.5"]
-      doc.install Dir["man/*.html"]
     end
   end
 
